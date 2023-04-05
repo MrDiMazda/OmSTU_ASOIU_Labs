e366ce8db18193da01cf561771a262f2af0506b2
MrDiMazda <channelborsh@gmail.com>
2023-04-05 16:21:35 +0600

    Corrected mistakes

--- Lab1.py
+++ Lab1.py
@@ -3,14 +3,12 @@ from bs4 import BeautifulSoup
 
 def GetParsedPage(url):
     page = requests.get(url)
-
     soup = BeautifulSoup(page.text, "html.parser")
     return soup
 
 def GetFilmsNames(soup):
     data = soup.find("tbody").findAll("tr")
     toplist=[]
-
     for tag in data:
         movie=tag.find("td",{"class":"titleColumn"}).find("a").text
         rating=tag.find("td",{"class":"ratingColumn imdbRating"}).find("strong").text
