13320f15f0d58e37b089cf2e461dceb18e4ffbe8
MrDiMazda <channelborsh@gmail.com>
2023-04-05 16:01:40 +0600

    Edited Lab1.py

--- Lab1.py
+++ Lab1.py
@@ -1,17 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.imdb.com/chart/top/?ref_=nv_mp_mv250"
-page = requests.get(url)
+def GetParsedPage(url):
+    page = requests.get(url)
 
-soup = BeautifulSoup(page.text, "html.parser")
-data = soup.find("tbody").findAll("tr")
+    soup = BeautifulSoup(page.text, "html.parser")
+    return soup
 
-toplist=[]
+def GetFilmsNames(soup):
+    data = soup.find("tbody").findAll("tr")
+    toplist=[]
 
-for tag in data:
-    movie=tag.find("td",{"class":"titleColumn"}).find("a").text
-    rating=tag.find("td",{"class":"ratingColumn imdbRating"}).find("strong").text
-    toplist.append({"Title":movie,"Rating":rating})
+    for tag in data:
+        movie=tag.find("td",{"class":"titleColumn"}).find("a").text
+        rating=tag.find("td",{"class":"ratingColumn imdbRating"}).find("strong").text
+        toplist.append({"Title":movie,"Rating":rating})
+    
+    return toplist
 
-print(toplist)
+if __name__=="__main__":
+    url = "https://www.imdb.com/chart/top/?ref_=nv_mp_mv250"
+    parsedPage = GetParsedPage(url)
+    topList = GetFilmsNames(parsedPage)
+    print(topList)
