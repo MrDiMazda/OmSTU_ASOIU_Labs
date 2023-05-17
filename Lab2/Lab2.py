02fddb1e274698344c7228aad7af61efeb779722
MrDiMazda <channelborsh@gmail.com>
2023-05-17 15:54:28 +0600

    Edited for main task

--- Lab2/Lab2.py
+++ Lab2/Lab2.py
@@ -1,79 +1,91 @@
-import requests, json, aiogram, logging
+import requests, json, aiogram, logging, time, asyncio
 
-API_TOKEN = "6084681905:AAGf5WJsfuCDTfWacP-iwoY4_OTIf9EGyCs"
+API_TOKEN = "6266863305:AAFKWnKGJzvX-Kbri2sWG6tpmTl1wKm3V4s"
 
 logging.basicConfig(level=logging.INFO)
 
 bot = aiogram.Bot(token=API_TOKEN)
 dp = aiogram.Dispatcher(bot)
 
-mode = 0
-FromCurrency = ""
-ToCurrency = ""
+mode = -1
+working = True
 
-templates = []
-buttons = ["USD","EUR","CNY","RUB"]
-@dp.message_handler(lambda message: (message.text == 'Новый шаблон') or (message.text in buttons))
-async def CreateTemplate(message = aiogram.types.Message): #    Создание шаблона
-    global mode,FromCurrency,ToCurrency
-    keyboard = aiogram.types.ReplyKeyboardMarkup(resize_keyboard=True)
-    if mode == 0:
-        keyboard.add(*buttons)
-        mode = 1
-
-        await message.answer("Выберите рассматриваемую валюту:",reply_markup=keyboard)
-    elif mode == 1:
-        FromCurrency = message.text
-        keyboard.add(*[currency for currency in buttons if currency!=message.text])
-        mode = 2
-        
-        await message.answer("Выберите валюту, в которую хотите перевести:",reply_markup=keyboard)
-    else:
-        keyboard.add("Главная")
-        ToCurrency = message.text
-        with open(f"{message.from_user.username}.txt",'r+') as file:
-            mode = 0
-            newTemplate = f"{FromCurrency} - {ToCurrency}\n"
-            if newTemplate in file.readlines():
-                keyboard.add("Новый шаблон")
-                await message.answer("Этот шаблон у вас существует!",reply_markup=keyboard)
-            else:
-                file.writelines(newTemplate)
-                await message.answer("Шаблон добавлен!",reply_markup=keyboard)
-
-@dp.message_handler(lambda message:message.text == "Главная")
-async def MainMenu(message = aiogram.types.Message): #  Главное меню
-    keyboard = aiogram.types.ReplyKeyboardMarkup(resize_keyboard=True)
-    with open(f"{message.from_user.username}.txt",'r+') as file:
-        templates=[]
-        rawtemplates = file.readlines()
-        for template in rawtemplates:templates.append(template.split("\n")[0])
-    keyboard.add(*templates)
-    keyboard.add("Новый шаблон")
-
-    await message.answer("Выберите шаблон или создайте новый:",reply_markup=keyboard)
-
-@dp.message_handler(lambda message: message.text in templates)
-async def CurrentExchangeRate(message = aiogram.types.Message): #   Вывод курса валют
-    from_currency,to_currency=map(str,message.text.split("\n")[0].split(" - "))
-    baseUrl=r"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE"
-    mainUrl=baseUrl + f"&from_currency={from_currency}&to_currency={to_currency}&apikey=TCV8VLPF0AYONHB3"
-    result = requests.get(mainUrl).json()
-
-    price = round(float(result["Realtime Currency Exchange Rate"]["5. Exchange Rate"]),2)
-
-    await message.reply(f"1 {from_currency} = {price} {to_currency}")
-
-@dp.message_handler(commands=['start','help'])
-async def send_welcome(message: aiogram.types.Message): #   Информация о боте
-    global buttons
-    await message.answer("""
+@dp.message_handler(commands=['start','help']) 
+async def HelpMessage(message: aiogram.types.Message):
+    global mode, exchange_max, exchange_min
+    await message.reply('''
     Привет!
-Я бот, информирующий о любых курсах валют!
-    """)
+Я бот, мониторящий изменение курса доллара за пределы Вами проставленных границ!
+    ''')
     try:
-        file = open(f"{message.from_user.username}.txt")
-        if file.readlines() == []: await CreateTemplate(message)
-        else: await MainMenu(message)
-        file.close()
-    except FileNotFoundError: await CreateTemplate(message)
+        with open(f"{message.from_user.id}.txt") as file:
+            global data
+            data = json.load(file)
+            exchange_min = data["min"]
+            exchange_max = data["max"]
+            await Monitoring(message)
+    except json.decoder.JSONDecodeError:
+        mode = 0
+        await CreateBorders(message)
+    except FileNotFoundError:
+        f = open(f"{message.from_user.id}.txt",'w+')
+        f.close()
+        mode = 0
+        await CreateBorders(message)
+
+@dp.message_handler(lambda message: message.text in ["Изменить нижний предел","Изменить верхний предел"] or message.text.isdecimal() == True)
+async def CreateBorders(message: aiogram.types.Message):
+    global mode, exchange_min, exchange_max, working, emin
+    working = False
+    keyboard = aiogram.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add("Главная")
+    if message.text in ["Изменить нижний предел","Изменить верхний предел"]:
+        mode = 0
+        if message.text == "Изменить верхний предел": mode = 1
+    if mode == 0:
+        mode = (2 if message.text == "Изменить нижний предел" else 1)
+        emin = (1 if message.text == "Изменить нижний предел" else 0)
+        await message.answer("Выберите нижний предел:",reply_markup=aiogram.types.ReplyKeyboardRemove())
+    
+    elif mode == 1:
+        mode = 2
+        if message.text.isdecimal(): exchange_min = message.text
+        await message.answer("Выберите верхний предел:",reply_markup=aiogram.types.ReplyKeyboardRemove())
+
+    elif mode == 2:
+        mode = -1
+        if message.text.isdecimal():
+            if emin == 1:
+                exchange_min = message.text
+                emin = 0
+            else: exchange_max = message.text
+        with open(f"{message.from_user.id}.txt","r+") as file:
+            data = {
+                "min": exchange_min,
+                "max": exchange_max
+            }
+            json.dump(data,file)
+            await message.answer("Новые пределы заданы!",reply_markup=keyboard)
+            working = True
+
+@dp.message_handler(lambda message: message.text == "Главная")
+async def Monitoring(message: aiogram.types.Message):
+    global working
+    kboard = aiogram.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    kboard.add(*["Изменить нижний предел","Изменить верхний предел"])
+    while working:
+        print('sas')
+        baseUrl=r"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE"
+        mainUrl=baseUrl + f"&from_currency=USD&to_currency=RUB&apikey=TCV8VLPF0AYONHB3"
+        result = requests.get(mainUrl).json()
+
+        price = round(float(result["Realtime Currency Exchange Rate"]["5. Exchange Rate"]),2)
+        msg = f"1 USD = {price} RUB."
+        with open(f"{message.from_user.id}.txt","r") as file:
+            data = json.load(file)
+        if float(price) < float(data["min"]):
+            msg += f" Стоимость ниже границы ({data['min']})"
+        elif float(price) > float(data["max"]):
+            msg += f" Стоимость выше границы ({data['max']})"
+        await message.answer(msg,reply_markup=kboard)
+        await asyncio.sleep(30)
